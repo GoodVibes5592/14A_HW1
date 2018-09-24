7f42729848e79db5fa68289fed1e369dc598ee68
GoodVibes5592 <jhines.psu@gmail.com>
2018-09-24 17:41:00 -0400

    update routes

--- routes.py
+++ routes.py
@@ -68,4 +68,5 @@ if __name__ == '__main__':
     # ========== End of Part 2.2 ==========
     # start API
     #app.run(host='0.0.0.0', port=8000, debug=True)
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    #app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(debug=True)
